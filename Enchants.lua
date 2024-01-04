b498b926e6c5a0d3de35a22c9f16d9257b8d4b6a
Zachary Hirsch <zhirsch@umich.edu>
2024-01-03 21:01:57 -0500

    Add Medium Armor Kit enchant

--- Enchants.lua
+++ Enchants.lua
@@ -1,5 +1,9 @@
 -- TODO: Disambiguate enchants that have the same text (and therefore appear multiple times in this table).
 local map = {
+    [16]   = { [INVSLOT_CHEST]    = "item:2313",
+               [INVSLOT_LEGS]     = "item:2313",
+               [INVSLOT_HAND]     = "item:2313",
+               [INVSLOT_FEET]     = "item:2313" },
     [17]   = { [INVSLOT_CHEST]    = "item:4265",
                [INVSLOT_LEGS]     = "item:4265",
                [INVSLOT_HAND]     = "item:4265",
