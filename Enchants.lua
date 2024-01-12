05813b7a1c184062f52d2f6cb78f13fc3edaa7a2
Zachary Hirsch <zhirsch@umich.edu>
2024-01-11 21:10:30 -0500

    Update Enchants.lua

--- Enchants.lua
+++ Enchants.lua
@@ -15,44 +15,13 @@ if IsClassic() or IsTBC() then
         ["item:38771"] = "spell:7457",
         ["item:38773"] = "spell:7748",
         ["item:38786"] = "spell:7867",
+        ["item:38789"] = "spell:13419",
         ["item:38794"] = "spell:13503",
+        ["item:38797"] = "spell:13536",
         ["item:38804"] = "spell:13626",
         ["item:38817"] = "spell:7782",
     }
 end
-
---[[
-MCS.Enchant = {
-    new = function(link, icon, tooltip)
-        local self  = {
-            link    = link,
-            icon    = icon,
-            tooltip = tooltip,
-        }
-        local cls   = { __index = MCS.Enchant.prototype }
-        return setmetatable(self, cls)
-    end,
-    compat = function(slot, itemLink)
-        local link = Moldy.Enchants:get(itemLink, slot)
-        if not link then
-            return nil
-        end
-        local icon, tooltip
-        if string.match(link, "item:(%d+)") then
-            icon = GetItemIcon(link)
-            tooltip = nil
-        elseif string.match(link, "spell:(%d+)") then
-            icon = GetSpellTexture(spellId)
-            tooltip = nil
-        elseif string.match(link, "enchant:(%d+)") then
-            icon = 134400
-            tooltip = link
-        end
-        return MCS.Enchant.new(link, icon, tooltip)
-    end,
-}
-MCS.Enchant.prototype = {}
-]]
 
 local Enchants
 Enchants = {
@@ -63,41 +32,23 @@ Enchants = {
     end,
 }
 Enchants.prototype = {
-    add = function(self, enchantId, link, slots, predicate)
-        if not self.map[enchantId] then
-            self.map[enchantId] = {}
-        end
-        for _, slot in ipairs(slots) do
-            if not self.map[enchantId][slot] then
-                self.map[enchantId][slot] = {}
-            end
-            table.insert(self.map[enchantId][slot], {
-                link      = Scrolls[link] or link,
-                predicate = predicate or function(...) return true end,
-            })
-        end
+    add = function(self, enchantId, link, predicate)
+        local enchants = GetOrCreateTableEntry(self.map, enchantId, {})
+        table.insert(enchants, {
+            link      = Scrolls[link] or link,
+            predicate = predicate or function(...) return true end,
+        })
     end,
     get = function(self, link, slot)
         local enchantId = string.match(link, "item:%d+:(%d+):")
-        if not enchantId then
-            return nil
-        end
-        local slots = self.map[tonumber(enchantId)]
-        if not slots then
-            Moldy:Printf("Unknown enchant %s on item %s", enchantId, link)
-            return "enchant:"..enchantId
-        end
-        local enchants = slots[slot]
-        if not enchants then
+        if not enchantId then return nil end
+        local enchants = GetOrCreateTableEntry(self.map, tonumber(enchantId), {})
+        local _, enchant = FindInTableIf(enchants, function(e) return e.predicate(link, slot) end)
+        if not enchant then
             Moldy:Printf("Unknown enchant %s in slot %d on item %s", enchantId, slot, link)
             return "enchant:"..enchantId
         end
-        for _, enchant in ipairs(enchants) do
-            if enchant.predicate(link) then
-                return enchant.link
-            end
-        end
-        return "enchant:"..enchantId
+        return enchant.link
     end,
 }
 
@@ -127,7 +78,6 @@ Moldy.Enchants.map = {
     [ 783] = { ["*"] = "spell:7771"  },
     [ 804] = { ["*"] = "spell:13522" },
     [ 805] = { ["*"] = "spell:13943" },
-    [ 823] = { ["*"] = "spell:13536" },
     [ 843] = { ["*"] = "spell:13607" },
     [ 844] = { ["*"] = "spell:13612" },
     [ 845] = { ["*"] = "spell:13617" },
@@ -302,139 +252,156 @@ Moldy.Enchants.map = {
 }
 ]]
 
-local function IsTwoHandedWeapon(link)
+local function Or(...)
+    local predicates = {...}
+    return function(...)
+        for _, predicate in ipairs(predicates) do
+            if predicate(...) then
+                return true
+            end
+        end
+        return false
+    end
+end
+
+local function Slot(allowed)
+    return function(link, slot) return allowed == slot end
+end
+
+local function IsTwoHandedWeapon(link, slot)
     return false
 end
 
-Moldy.Enchants:add(  16, "item:2313",   {INVSLOT_CHEST, INVSLOT_LEGS, INVSLOT_HAND, INVSLOT_FEET})
-Moldy.Enchants:add(  17, "item:4265",   {INVSLOT_CHEST, INVSLOT_LEGS, INVSLOT_HAND, INVSLOT_FEET})
-Moldy.Enchants:add(  24, "item:38769",  {INVSLOT_CHEST})
-Moldy.Enchants:add(  41, "item:38766",  {INVSLOT_CHEST})
-Moldy.Enchants:add(  41, "item:38679",  {INVSLOT_WRIST})
-Moldy.Enchants:add(  44, "item:38767",  {INVSLOT_CHEST})
-Moldy.Enchants:add(  63, "item:38798",  {INVSLOT_CHEST})
-Moldy.Enchants:add(  65, "item:38770",  {INVSLOT_BACK})
-Moldy.Enchants:add(  66, "item:38785",  {INVSLOT_FEET})
-Moldy.Enchants:add(  66, "item:38771",  {INVSLOT_WRIST})
-Moldy.Enchants:add(  66, "item:38787",  {INVSLOT_OFFHAND})
-Moldy.Enchants:add( 241, "item:38772",  {INVSLOT_MAINHAND, INVSLOT_OFFHAND}, IsTwoHandedWeapon)
-Moldy.Enchants:add( 241, "item:38794",  {INVSLOT_MAINHAND, INVSLOT_OFFHAND})
-Moldy.Enchants:add( 242, "item:38773",  {INVSLOT_CHEST})
-Moldy.Enchants:add( 247, "item:38789",  {INVSLOT_BACK})
-Moldy.Enchants:add( 247, "item:38777",  {INVSLOT_WRIST})
-Moldy.Enchants:add( 247, "item:38786",  {INVSLOT_FEET})
-Moldy.Enchants:add( 248, "item:38817",  {INVSLOT_WRIST})
-Moldy.Enchants:add( 803, "item:38838",  {INVSLOT_MAINHAND, INVSLOT_OFFHAND})
-Moldy.Enchants:add( 846, "item:50816",  {INVSLOT_HAND})
-Moldy.Enchants:add( 846, "item:19971",  {INVSLOT_MAINHAND})
-Moldy.Enchants:add( 847, "item:38804",  {INVSLOT_CHEST})
-Moldy.Enchants:add( 911, "item:38837",  {INVSLOT_FEET})
-Moldy.Enchants:add( 983, "item:38976",  {INVSLOT_FEET})
-Moldy.Enchants:add( 983, "item:38959",  {INVSLOT_BACK})
-Moldy.Enchants:add(1075, "item:38966",  {INVSLOT_FEET})
-Moldy.Enchants:add(1099, "item:44457",  {INVSLOT_BACK})
-Moldy.Enchants:add(1128, "item:44455",  {INVSLOT_OFFHAND})
-Moldy.Enchants:add(1144, "item:38928",  {INVSLOT_CHEST})
-Moldy.Enchants:add(1147, "item:38961",  {INVSLOT_FEET})
-Moldy.Enchants:add(1147, "item:38980",  {INVSLOT_WRIST})
-Moldy.Enchants:add(1597, "item:44469",  {INVSLOT_FEET})
-Moldy.Enchants:add(1600, "item:38971",  {INVSLOT_WRIST})
-Moldy.Enchants:add(1883, "item:38852",  {INVSLOT_WRIST})
-Moldy.Enchants:add(1888, "item:38858",  {INVSLOT_BACK})
-Moldy.Enchants:add(1888, "item:38907",  {INVSLOT_OFFHAND})
-Moldy.Enchants:add(1891, "item:38865",  {INVSLOT_CHEST})
-Moldy.Enchants:add(1891, "item:38898",  {INVSLOT_WRIST})
-Moldy.Enchants:add(1900, "item:38873",  {INVSLOT_MAINHAND, INVSLOT_OFFHAND})
-Moldy.Enchants:add(1951, "item:38978",  {INVSLOT_BACK})
-Moldy.Enchants:add(1952, "item:38954",  {INVSLOT_OFFHAND})
-Moldy.Enchants:add(1953, "item:39002",  {INVSLOT_CHEST})
-Moldy.Enchants:add(2326, "item:38997",  {INVSLOT_WRIST})
-Moldy.Enchants:add(2332, "item:44470",  {INVSLOT_WRIST})
-Moldy.Enchants:add(2381, "item:38962",  {INVSLOT_CHEST})
-Moldy.Enchants:add(2564, "item:38890",  {INVSLOT_HAND})
-Moldy.Enchants:add(2564, "item:38880",  {INVSLOT_MAINHAND, INVSLOT_OFFHAND})
-Moldy.Enchants:add(2613, "item:38885",  {INVSLOT_HAND})
-Moldy.Enchants:add(2621, "item:38894",  {INVSLOT_BACK})
-Moldy.Enchants:add(2622, "item:38895",  {INVSLOT_BACK})
-Moldy.Enchants:add(2649, "item:38902",  {INVSLOT_WRIST})
-Moldy.Enchants:add(2649, "item:38909",  {INVSLOT_FEET})
-Moldy.Enchants:add(2650, "item:38903",  {INVSLOT_WRIST})
-Moldy.Enchants:add(2659, "item:38911",  {INVSLOT_CHEST})
-Moldy.Enchants:add(2661, "item:38913",  {INVSLOT_CHEST})
-Moldy.Enchants:add(2661, "item:38987",  {INVSLOT_WRIST})
-Moldy.Enchants:add(2669, "item:38921",  {INVSLOT_MAINHAND, INVSLOT_OFFHAND})
-Moldy.Enchants:add(2679, "item:38901",  {INVSLOT_WRIST})
-Moldy.Enchants:add(2723, "item:23765",  {INVSLOT_RANGED})
-Moldy.Enchants:add(2746, "item:24276",  {INVSLOT_LEGS})
-Moldy.Enchants:add(2748, "item:24274",  {INVSLOT_LEGS})
-Moldy.Enchants:add(2978, "item:28889",  {INVSLOT_SHOULDER})
-Moldy.Enchants:add(2995, "item:28909",  {INVSLOT_SHOULDER})
-Moldy.Enchants:add(2999, "item:29186",  {INVSLOT_HEAD})
-Moldy.Enchants:add(3002, "item:29191",  {INVSLOT_HEAD})
-Moldy.Enchants:add(3003, "item:29192",  {INVSLOT_HEAD})
-Moldy.Enchants:add(3011, "item:29534",  {INVSLOT_LEGS})
-Moldy.Enchants:add(3222, "item:38967",  {INVSLOT_HAND})
-Moldy.Enchants:add(3222, "item:38947",  {INVSLOT_MAINHAND, INVSLOT_OFFHAND})    
-Moldy.Enchants:add(3230, "item:38950",  {INVSLOT_BACK})
-Moldy.Enchants:add(3231, "spell:44484", {INVSLOT_HAND})
-Moldy.Enchants:add(3231, "spell:44598", {INVSLOT_WRIST})
-Moldy.Enchants:add(3234, "item:38953",  {INVSLOT_HAND})
-Moldy.Enchants:add(3244, "item:38974",  {INVSLOT_FEET})
-Moldy.Enchants:add(3246, "item:38979",  {INVSLOT_HAND})
-Moldy.Enchants:add(3252, "item:38989",  {INVSLOT_CHEST})
-Moldy.Enchants:add(3253, "item:38990",  {INVSLOT_HAND})
-Moldy.Enchants:add(3294, "item:39001",  {INVSLOT_BACK})
-Moldy.Enchants:add(3297, "item:39005",  {INVSLOT_CHEST})
-Moldy.Enchants:add(3326, "item:38372",  {INVSLOT_LEGS})
-Moldy.Enchants:add(3330, "item:38376",  {INVSLOT_HEAD, INVSLOT_CHEST, INVSLOT_SHOULDER, INVSLOT_LEGS, INVSLOT_HAND, INVSLOT_FEET})
-Moldy.Enchants:add(3368, "spell:53344", {INVSLOT_MAINHAND, INVSLOT_OFFHAND})
-Moldy.Enchants:add(3370, "spell:53343", {INVSLOT_MAINHAND, INVSLOT_OFFHAND})
-Moldy.Enchants:add(3594, "spell:54446", {INVSLOT_MAINHAND, INVSLOT_OFFHAND})
-Moldy.Enchants:add(3599, "spell:54736", {INVSLOT_WAIST})
-Moldy.Enchants:add(3601, "spell:54793", {INVSLOT_WAIST})
-Moldy.Enchants:add(3604, "spell:54999", {INVSLOT_HAND})
-Moldy.Enchants:add(3605, "item:41111",  {INVSLOT_BACK})
-Moldy.Enchants:add(3606, "item:41118",  {INVSLOT_FEET})
-Moldy.Enchants:add(3719, "item:41602",  {INVSLOT_LEGS})
-Moldy.Enchants:add(3721, "item:41604",  {INVSLOT_LEGS})
-Moldy.Enchants:add(3722, "spell:55642", {INVSLOT_BACK})
-Moldy.Enchants:add(3730, "spell:55777", {INVSLOT_CHEST})
-Moldy.Enchants:add(3758, "spell:57691", {INVSLOT_WRIST})
-Moldy.Enchants:add(3788, "item:44497",  {INVSLOT_MAINHAND, INVSLOT_OFFHAND})
-Moldy.Enchants:add(3789, "item:44493",  {INVSLOT_MAINHAND, INVSLOT_OFFHAND})
-Moldy.Enchants:add(3790, "item:43987",  {INVSLOT_MAINHAND, INVSLOT_OFFHAND})
-Moldy.Enchants:add(3797, "item:44075",  {INVSLOT_HEAD})
-Moldy.Enchants:add(3808, "item:50335",  {INVSLOT_SHOULDER})
-Moldy.Enchants:add(3809, "item:50336",  {INVSLOT_SHOULDER})
-Moldy.Enchants:add(3810, "item:50338",  {INVSLOT_SHOULDER})
-Moldy.Enchants:add(3811, "item:50337",  {INVSLOT_SHOULDER})
-Moldy.Enchants:add(3812, "item:44884",  {INVSLOT_HEAD})
-Moldy.Enchants:add(3817, "item:50367",  {INVSLOT_HEAD})
-Moldy.Enchants:add(3818, "item:50369",  {INVSLOT_HEAD})
-Moldy.Enchants:add(3819, "item:50370",  {INVSLOT_HEAD})
-Moldy.Enchants:add(3820, "item:50368",  {INVSLOT_HEAD})
-Moldy.Enchants:add(3822, "item:38373",  {INVSLOT_LEGS})
-Moldy.Enchants:add(3823, "item:38374",  {INVSLOT_LEGS})
-Moldy.Enchants:add(3824, "item:44449",  {INVSLOT_FEET})
-Moldy.Enchants:add(3825, "item:44456",  {INVSLOT_BACK})
-Moldy.Enchants:add(3826, "item:38986",  {INVSLOT_FEET})
-Moldy.Enchants:add(3827, "item:44463",  {INVSLOT_MAINHAND, INVSLOT_OFFHAND})
-Moldy.Enchants:add(3828, "item:38992",  {INVSLOT_MAINHAND, INVSLOT_OFFHAND})
-Moldy.Enchants:add(3829, "item:38964",  {INVSLOT_HAND})
-Moldy.Enchants:add(3831, "item:39003",  {INVSLOT_BACK})
-Moldy.Enchants:add(3832, "item:44465",  {INVSLOT_CHEST})
-Moldy.Enchants:add(3834, "item:44467",  {INVSLOT_MAINHAND, INVSLOT_OFFHAND})
-Moldy.Enchants:add(3838, "spell:61120", {INVSLOT_SHOULDER})
-Moldy.Enchants:add(3840, "spell:44636", {INVSLOT_FINGER1, INVSLOT_FINGER2})
-Moldy.Enchants:add(3845, "item:44815",  {INVSLOT_WRIST})
-Moldy.Enchants:add(3847, "spell:62158", {INVSLOT_MAINHAND, INVSLOT_OFFHAND})
-Moldy.Enchants:add(3849, "item:44936",  {INVSLOT_OFFHAND})
-Moldy.Enchants:add(3850, "item:44947",  {INVSLOT_WRIST})
-Moldy.Enchants:add(3852, "item:44957",  {INVSLOT_SHOULDER})
-Moldy.Enchants:add(3854, "item:45056",  {INVSLOT_MAINHAND})
-Moldy.Enchants:add(3855, "item:45060",  {INVSLOT_MAINHAND, INVSLOT_OFFHAND})
-Moldy.Enchants:add(3859, "spell:63765", {INVSLOT_BACK})
-Moldy.Enchants:add(3860, "spell:63770", {INVSLOT_HAND})
-Moldy.Enchants:add(3869, "item:46026",  {INVSLOT_MAINHAND, INVSLOT_OFFHAND})
-Moldy.Enchants:add(3872, "spell:56039", {INVSLOT_LEGS})
-Moldy.Enchants:add(3873, "spell:56034", {INVSLOT_LEGS})
+Moldy.Enchants:add(  16, "item:2313")
+Moldy.Enchants:add(  17, "item:4265")
+Moldy.Enchants:add(  24, "item:38769")
+Moldy.Enchants:add(  41, "item:38679",  Slot(INVSLOT_WRIST))
+Moldy.Enchants:add(  41, "item:38766",  Slot(INVSLOT_CHEST))
+Moldy.Enchants:add(  44, "item:38767")
+Moldy.Enchants:add(  63, "item:38798")
+Moldy.Enchants:add(  65, "item:38770")
+Moldy.Enchants:add(  66, "item:38771",  Slot(INVSLOT_WRIST))
+Moldy.Enchants:add(  66, "item:38785",  Slot(INVSLOT_FEET))
+Moldy.Enchants:add(  66, "item:38787",  Slot(INVSLOT_OFFHAND))
+Moldy.Enchants:add( 241, "item:38772",  IsTwoHandedWeapon)
+Moldy.Enchants:add( 241, "item:38794")
+Moldy.Enchants:add( 242, "item:38773")
+Moldy.Enchants:add( 247, "item:38777",  Slot(INVSLOT_WRIST))
+Moldy.Enchants:add( 247, "item:38786",  Slot(INVSLOT_FEET))
+Moldy.Enchants:add( 247, "item:38789",  Slot(INVSLOT_BACK))
+Moldy.Enchants:add( 248, "item:38817")
+Moldy.Enchants:add( 803, "item:38838")
+Moldy.Enchants:add( 823, "item:38797")
+Moldy.Enchants:add( 846, "item:19971",  Slot(INVSLOT_MAINHAND))
+Moldy.Enchants:add( 846, "item:50816",  Slot(INVSLOT_HAND))
+Moldy.Enchants:add( 847, "item:38804")
+Moldy.Enchants:add( 911, "item:38837")
+Moldy.Enchants:add( 983, "item:38959",  Slot(INVSLOT_BACK))
+Moldy.Enchants:add( 983, "item:38976",  Slot(INVSLOT_FEET))
+Moldy.Enchants:add(1075, "item:38966")
+Moldy.Enchants:add(1099, "item:44457")
+Moldy.Enchants:add(1128, "item:44455")
+Moldy.Enchants:add(1144, "item:38928")
+Moldy.Enchants:add(1147, "item:38961",  Slot(INVSLOT_FEET))
+Moldy.Enchants:add(1147, "item:38980",  Slot(INVSLOT_WRIST))
+Moldy.Enchants:add(1597, "item:44469")
+Moldy.Enchants:add(1600, "item:38971")
+Moldy.Enchants:add(1883, "item:38852")
+Moldy.Enchants:add(1888, "item:38858",  Slot(INVSLOT_BACK))
+Moldy.Enchants:add(1888, "item:38907",  Slot(INVSLOT_OFFHAND))
+Moldy.Enchants:add(1891, "item:38865",  Slot(INVSLOT_CHEST))
+Moldy.Enchants:add(1891, "item:38898",  Slot(INVSLOT_WRIST))
+Moldy.Enchants:add(1900, "item:38873")
+Moldy.Enchants:add(1951, "item:38978")
+Moldy.Enchants:add(1952, "item:38954")
+Moldy.Enchants:add(1953, "item:39002")
+Moldy.Enchants:add(2326, "item:38997")
+Moldy.Enchants:add(2332, "item:44470")
+Moldy.Enchants:add(2381, "item:38962")
+Moldy.Enchants:add(2564, "item:38880",  Or(Slot(INVSLOT_MAINHAND), Slot(INVSLOT_OFFHAND)))
+Moldy.Enchants:add(2564, "item:38890",  Slot(INVSLOT_HAND))
+Moldy.Enchants:add(2613, "item:38885")
+Moldy.Enchants:add(2621, "item:38894")
+Moldy.Enchants:add(2622, "item:38895")
+Moldy.Enchants:add(2649, "item:38902",  Slot(INVSLOT_WRIST))
+Moldy.Enchants:add(2649, "item:38909",  Slot(INVSLOT_FEET))
+Moldy.Enchants:add(2650, "item:38903")
+Moldy.Enchants:add(2659, "item:38911")
+Moldy.Enchants:add(2661, "item:38913",  Slot(INVSLOT_CHEST))
+Moldy.Enchants:add(2661, "item:38987",  Slot(INVSLOT_WRIST))
+Moldy.Enchants:add(2669, "item:38921")
+Moldy.Enchants:add(2679, "item:38901")
+Moldy.Enchants:add(2723, "item:23765")
+Moldy.Enchants:add(2746, "item:24276")
+Moldy.Enchants:add(2748, "item:24274")
+Moldy.Enchants:add(2978, "item:28889")
+Moldy.Enchants:add(2995, "item:28909")
+Moldy.Enchants:add(2999, "item:29186")
+Moldy.Enchants:add(3002, "item:29191")
+Moldy.Enchants:add(3003, "item:29192")
+Moldy.Enchants:add(3011, "item:29534")
+Moldy.Enchants:add(3222, "item:38947",  Or(Slot(INVSLOT_MAINHAND), Slot(INVSLOT_OFFHAND)))
+Moldy.Enchants:add(3222, "item:38967",  Slot(INVSLOT_HAND))
+Moldy.Enchants:add(3230, "item:38950")
+Moldy.Enchants:add(3231, "item:38951")
+Moldy.Enchants:add(3231, "item:38984")
+Moldy.Enchants:add(3234, "item:38953")
+Moldy.Enchants:add(3244, "item:38974")
+Moldy.Enchants:add(3246, "item:38979")
+Moldy.Enchants:add(3252, "item:38989")
+Moldy.Enchants:add(3253, "item:38990")
+Moldy.Enchants:add(3294, "item:39001")
+Moldy.Enchants:add(3297, "item:39005")
+Moldy.Enchants:add(3326, "item:38372")
+Moldy.Enchants:add(3330, "item:38376")
+Moldy.Enchants:add(3368, "spell:53344")
+Moldy.Enchants:add(3370, "spell:53343")
+Moldy.Enchants:add(3594, "spell:54446")
+Moldy.Enchants:add(3599, "spell:54736")
+Moldy.Enchants:add(3601, "spell:54793")
+Moldy.Enchants:add(3604, "spell:54999")
+Moldy.Enchants:add(3605, "item:41111")
+Moldy.Enchants:add(3606, "item:41118")
+Moldy.Enchants:add(3719, "item:41602")
+Moldy.Enchants:add(3721, "item:41604")
+Moldy.Enchants:add(3722, "spell:55642")
+Moldy.Enchants:add(3730, "spell:55777")
+Moldy.Enchants:add(3758, "spell:57691")
+Moldy.Enchants:add(3788, "item:44497")
+Moldy.Enchants:add(3789, "item:44493")
+Moldy.Enchants:add(3790, "item:43987")
+Moldy.Enchants:add(3797, "item:44075")
+Moldy.Enchants:add(3808, "item:50335")
+Moldy.Enchants:add(3809, "item:50336")
+Moldy.Enchants:add(3810, "item:50338")
+Moldy.Enchants:add(3811, "item:50337")
+Moldy.Enchants:add(3812, "item:44884")
+Moldy.Enchants:add(3817, "item:50367")
+Moldy.Enchants:add(3818, "item:50369")
+Moldy.Enchants:add(3819, "item:50370")
+Moldy.Enchants:add(3820, "item:50368")
+Moldy.Enchants:add(3822, "item:38373")
+Moldy.Enchants:add(3823, "item:38374")
+Moldy.Enchants:add(3824, "item:44449")
+Moldy.Enchants:add(3825, "item:44456")
+Moldy.Enchants:add(3826, "item:38986")
+Moldy.Enchants:add(3827, "item:44463")
+Moldy.Enchants:add(3828, "item:38992")
+Moldy.Enchants:add(3829, "item:38964")
+Moldy.Enchants:add(3831, "item:39003")
+Moldy.Enchants:add(3832, "item:44465")
+Moldy.Enchants:add(3834, "item:44467")
+Moldy.Enchants:add(3838, "spell:61120")
+Moldy.Enchants:add(3840, "spell:44636")
+Moldy.Enchants:add(3845, "item:44815")
+Moldy.Enchants:add(3847, "spell:62158")
+Moldy.Enchants:add(3849, "item:44936")
+Moldy.Enchants:add(3850, "item:44947")
+Moldy.Enchants:add(3852, "item:44957")
+Moldy.Enchants:add(3854, "item:45056")
+Moldy.Enchants:add(3855, "item:45060")
+Moldy.Enchants:add(3859, "spell:63765")
+Moldy.Enchants:add(3860, "spell:63770")
+Moldy.Enchants:add(3869, "item:46026")
+Moldy.Enchants:add(3872, "spell:56039")
+Moldy.Enchants:add(3873, "spell:56034")
